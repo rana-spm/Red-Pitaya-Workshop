87c097fe8c8a0c2dc93204981329241e3dd9c77e
PierreBeur <cbeurtheret@ucsd.edu>
2023-02-26 02:58:42 -0800

    Create New GUI

--- hackathon/gui_server/static/gui.js
+++ hackathon/gui_server/static/gui.js
@@ -1,41 +1,50 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const rxForm = document.querySelector('#rx');
-    const txForm = document.querySelector('#tx');
+  const messages = document.querySelector('main');
+  const form = document.querySelector('form');
+  const input = document.querySelector('input');
 
-    const messages = document.querySelector('#messages');
-    const rxWaiting = document.querySelector('#rx #waiting');
+  const numBuffers = 100;
+  async function rx() {
+    return await (await fetch(`/rx/${numBuffers}`)).json();
+  }
 
-    rxForm.addEventListener('submit', async (event) => {
-        event.preventDefault();
-        rxWaiting.style = ''; // Show waiting text
-        // Request message from server
-        const numBuffers = rxForm.querySelector('input').value;
-        const message = await (await fetch(`/rx/${numBuffers}`)).json();
-        rxWaiting.style.display = 'none'; // Hide waiting text
-        // Add message to chat log
-        const newMessage = document.createElement('div');
-        newMessage.className = 'message';
-        const time = (new Date()).toLocaleTimeString([], {hour12: false});
-        newMessage.textContent = time + ': ' + message;
-        messages.appendChild(newMessage);
-    });
+  async function getMessage() {
+    // Request message from server
+    const message = await rx();
+    // Add message to chat log
+    addMessage(message);
+  }
 
-    txForm.addEventListener('submit', async (event) => {
-        event.preventDefault();
-        // Send message to server
-        const message = txForm.querySelector('input').value;
-        fetch('/tx', {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(message)
-        });
-        // Add message to chat log
-        const newMessage = document.createElement('div');
-        newMessage.className = 'message';
-        newMessage.classList.add('tx-message');
-        const time = (new Date()).toLocaleTimeString([], {hour12: false});
-        newMessage.textContent = time + ': ' + message;
-        messages.appendChild(newMessage);
-        txForm.reset();
-    });
+  async function tx(message) {
+    // Transmit message to server
+    await (await fetch('/tx', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(message)
+    })).json();
+    // Request message from server
+    getMessage();
+  }
+
+  function addMessage(message, self = false) {
+    const newMessage = document.createElement('div');
+    if (self) newMessage.className = 'self-message';
+    const time = (new Date()).toLocaleTimeString([], {hour12: false});
+    newMessage.textContent = `[${time}] ${message}`;
+    messages.appendChild(newMessage);
+  }
+
+  form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    // Get message from text input
+    const message = input.value;
+    if (message) {
+      // Transmit message to server
+      await tx(message);
+      // Add message to chat log
+      addMessage(message, true);
+      // Clear text input
+      form.reset();
+    }
+  });
 });
