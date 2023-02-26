f4562db2ecab99316bfdd6b50595b4e15f7d806e
PierreBeur <cbeurtheret@ucsd.edu>
2023-02-26 01:49:11 -0800

    Update GUI

--- hackathon/gui_server/static/gui.js
+++ hackathon/gui_server/static/gui.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const rxForm = document.querySelector('#rx');
     const txForm = document.querySelector('#tx');
 
-    const rxMessages = document.querySelector('#rx #messages');
+    const messages = document.querySelector('#messages');
     const rxWaiting = document.querySelector('#rx #waiting');
 
     rxForm.addEventListener('submit', async (event) => {
@@ -17,18 +17,25 @@ document.addEventListener('DOMContentLoaded', () => {
         newMessage.className = 'message';
         const time = (new Date()).toLocaleTimeString([], {hour12: false});
         newMessage.textContent = time + ': ' + message;
-        rxMessages.appendChild(newMessage);
-        rxForm.reset();
+        messages.appendChild(newMessage);
     });
 
     txForm.addEventListener('submit', async (event) => {
         event.preventDefault();
+        // Send message to server
         const message = txForm.querySelector('input').value;
         fetch('/tx', {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify(message)
         });
+        // Add message to chat log
+        const newMessage = document.createElement('div');
+        newMessage.className = 'message';
+        newMessage.classList.add('tx-message');
+        const time = (new Date()).toLocaleTimeString([], {hour12: false});
+        newMessage.textContent = time + ': ' + message;
+        messages.appendChild(newMessage);
         txForm.reset();
     });
 });
